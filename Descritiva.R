d15a4ce4f8443bf2833a125434f437872fdd2a45
Fernanda Waltrs Freitas <96556228+fwaltrs@users.noreply.github.com>
2024-08-14 20:48:09 -0300

    Update Descritiva.R

--- Descritiva.R
+++ Descritiva.R
@@ -7,11 +7,9 @@ library(gridExtra)
 
 
 # Importando a base -------------------------------------------------------------------
-caminho= "H:/Meu Drive/9º SEMESTRE/TG/bases/base_V7.parquet"
-base <- arrow::read_parquet(caminho)
+load("caminho")
 
-
-#### Verificamos as variáveis mais significativas isoladamente com a Y
+# Verificamos as variáveis mais significativas isoladamente com a Y
 matriz <- matrix(data=NA, ncol=2,nrow=length(5:219))
 for (i in 5:ncol(base)-1){
   modelo <- glm(var_resposta ~ ., data=base[,c(4,i)],family="binomial")
@@ -24,7 +22,6 @@ data1 = data11 %>% arrange(V2)
 
 
 # Análise da taxa de inadimplencia por período
-
 
 order <- c('dez21', 'fev22', 'mai22', 'jul22', 'set22')
 base$foto <- factor(base$foto, levels=order, ordered=TRUE)
@@ -48,20 +45,20 @@ ggplot(df, aes(x = Bad.foto, y = Bad.var_resposta, group = 1)) +
   theme_minimal() +  
   theme(
     plot.title = element_text(hjust = 0.5,size=18),
-    axis.title.x = element_text(size = 15),  # Aumenta o tamanho do título do eixo x
+    axis.title.x = element_text(size = 15),  
     axis.title.y = element_text(size = 15), 
     panel.border = element_rect(color = "black",fill=NA),
     axis.text.x = element_text(angle = 45, hjust = 1,size=12,color='black'),
     axis.text.y = element_text(size=12,color='black')
   )
 
-
 # Análise descritiva de auxílios com a variavel resposta ------------------
 
 table(base$var_resposta,base$CAT_var_275)
-# C0     C1     C2
-# 0 343342  41557   4452
-# 1  18311   3181    628
+
+
+base$CAT_var_275 <- ifelse(base$CAT_var_275 == "C0", "NA",
+                           ifelse(base$CAT_var_275 == "C1", "Sim", "Não"))
 
 cores = c("deepskyblue2","firebrick2")
 
@@ -77,39 +74,40 @@ gerar_df <- function(variavel){
 return(df)
   }
 
-
 g1 <- ggplot(gerar_df(base$CAT_var_275), aes(x = Categoria)) +
-  geom_bar(aes(y = Representatividade.Freq * 100), stat = "identity", fill = "deepskyblue2",width = 0.5) +
-  geom_line(aes(y = Taxa_Inadimplencia * 100, group = 1), color = "firebrick2", linewidth=1) +
-  geom_point(aes(y = Taxa_Inadimplencia * 100), color = "firebrick2",alpha=2,size=2) +
-  labs(x = "Auxílio", y = "Representatividade (%)", 
+  geom_bar(aes(y = Representatividade.Freq * 100), stat = "identity", fill = "deepskyblue2", width = 0.5) +
+  geom_line(aes(y = Taxa_Inadimplencia * 100, group = 1), color = "firebrick2", linewidth = 1) +
+  geom_point(aes(y = Taxa_Inadimplencia * 100), color = "firebrick2", alpha = 1, size = 2) +
+  labs(x = "Auxílio emergencial", y = "Representatividade (%)", 
        title = "Representatividade de auxílio\n e Taxa de Inadimplência") +
-  scale_y_continuous(sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal()+
+  scale_y_continuous(limits = c(0, 100), 
+                     sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) +
+  theme_minimal() +
   scale_fill_manual(values = cores) +  
   theme(
-    plot.title = element_text(hjust = 0.5,size=18),
-    axis.title.x = element_text(size = 15),  # Aumenta o tamanho do título do eixo x
+    plot.title = element_text(hjust = 0.5, size = 18),
+    axis.title.x = element_text(size = 15),  
     axis.title.y = element_text(size = 15), 
-    panel.border = element_rect(color = "black",fill=NA),
-    axis.text.x = element_text(size=12,color = "black"),
-    axis.text.y = element_text(size=12,color = "black")
+    panel.border = element_rect(color = "black", fill = NA),
+    axis.text.x = element_text(size = 12, color = "black"),
+    axis.text.y = element_text(size = 12, color = "black")
   )
 
 
-base$CAT_var_339 = ifelse(base$CAT_var_339==TRUE,1,0)
+base$var_339 = ifelse(base$var_339==TRUE,"Sim","Não")
 
-g2 <- ggplot(gerar_df(base$CAT_var_339), aes(x = Categoria)) +
-  geom_bar(aes(y = Representatividade.Freq * 100), stat = "identity", fill = "deepskyblue2",width = 0.5) +
+g2 <- ggplot(gerar_df(base$var_339), aes(x = Categoria)) +
+  geom_bar(aes(y = Representatividade.Freq * 100), stat = "identity", fill = "deepskyblue2",width = 0.35) +
   geom_line(aes(y = Taxa_Inadimplencia * 100, group = 1), color = "firebrick2", linewidth=1) +
   geom_point(aes(y = Taxa_Inadimplencia * 100), color = "firebrick2",size=2) +
   labs(x = "Endereço desfavorecido socialmente", y = "Representatividade (%)", 
-       title = "Representatividade de endereço desfavorecido\n e Taxa de Inadimplência") +
-  scale_y_continuous(sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal()+
+       title = "Representatividade de EDS \n e Taxa de Inadimplência") +
+  scale_y_continuous(limits = c(0,100),sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal()+
   scale_fill_manual(values = cores) +  
   theme(
     plot.title = element_text(hjust = 0.5,size = 18),
-    axis.title.x = element_text(size = 15),  # Aumenta o tamanho do título do eixo x
-    axis.title.y = element_text(size = 15),  # Aumenta o tamanho do título do eixo 
+    axis.title.x = element_text(size = 15), 
+    axis.title.y = element_text(size = 15),  
     panel.border = element_rect(color = "black",fill=NA),
     axis.text.x = element_text(size=12,color = "black"),
     axis.text.y = element_text(size=12,color = "black")
@@ -134,12 +132,12 @@ g3 <- ggplot(gerar_df(base$CAT_var_2), aes(x = Categoria)) +
   geom_point(aes(y = Taxa_Inadimplencia * 100), color = "firebrick2",size=2) +
   labs(x = "Idade", y = "Representatividade (%)", 
        title = "Representatividade de idade\n e Taxa de Inadimplência") +
-  scale_y_continuous(sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal()+
+  scale_y_continuous(limits = c(0,100),sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal()+
   scale_fill_manual(values = cores) +  
   theme(
     plot.title = element_text(hjust = 0.5,size = 18),
-    axis.title.x = element_text(size = 15),  # Aumenta o tamanho do título do eixo x
-    axis.title.y = element_text(size = 15),  # Aumenta o tamanho do título do eixo 
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),  
     panel.border = element_rect(color = "black",fill=NA),
     axis.text.x = element_text(angle = 45, hjust = 1,size=12,color='black'),
     axis.text.y = element_text(size=12,color = "black")
@@ -160,12 +158,12 @@ g4 <- ggplot(gerar_df(base$faixas_renda), aes(x = Categoria)) +
   geom_point(aes(y = Taxa_Inadimplencia * 100), color = "firebrick2",size=2) +
   labs(x = "Faixas de Renda", y = "Representatividade (%)", 
        title = "Representatividade de renda\n e Taxa de Inadimplência") +
-  scale_y_continuous(sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal()+
+  scale_y_continuous(limits = c(0,40),sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal()+
   scale_fill_manual(values = cores) +  
   theme(
     plot.title = element_text(hjust = 0.5,size=18),
-    axis.title.x = element_text(size = 15),  # Aumenta o tamanho do título do eixo x
-    axis.title.y = element_text(size = 15),  # Aumenta o tamanho do título do eixo 
+    axis.title.x = element_text(size = 15),  
+    axis.title.y = element_text(size = 15),  
     panel.border = element_rect(color = "black",fill=NA),
     axis.text.x = element_text(angle = 45, hjust = 1,size=12,color='black'),
     axis.text.y = element_text(size=12,color = "black")
@@ -177,8 +175,32 @@ grid.arrange(g3,g4,ncol=2)
 
 # Descritiva com relação a variáveis de comportamento ---------------------
 
-# observando variáveis e taxa de inadimplencia ----------------------------
+base$CAT_var_103 <- as.factor(ifelse(base$var_103  <= quantile(base$var_103, 0.25,na.rm=T), "0-6",
+                                            ifelse(base$var_103 <= quantile(base$var_103, 0.50,na.rm=T)  , "7-18", 
+                                                   ifelse(base$var_103 <= quantile(base$var_103, 0.75,na.rm=T)  ,"19-44", "45+"))))
 
+order <- c("0-6","7-18", "19-44", "45+" )
+base$CAT_var_103 <- factor(base$CAT_var_103, levels=order, ordered=TRUE)
+
+base$CAT_var_78 <-  as.factor(ifelse(is.na(base$var_78), "C0",
+                                   ifelse(base$var_78  <= quantile(base$var_78, 0.25,na.rm=T), "0-231",
+                                          ifelse(base$var_78 <= quantile(base$var_78, 0.50,na.rm=T)  , "232-786", 
+                                                 ifelse(base$var_78 <= quantile(base$var_78, 0.75,na.rm=T)  ,"787-2063", "2064+")))))
+
+
+order <- c("0-231","232-786", "787-2063","2064+"  )
+base$CAT_var_78 <- factor(base$CAT_var_78, levels=order, ordered=TRUE)
+
+base$CAT_var_22 <- as.factor(ifelse(is.na(base$var_22), "C0", 
+                                  ifelse(base$var_22 == median(base$var_22,na.rm = T),"Não", 'Sim')))
+
+
+base$CAT_var_57 <- as.factor(ifelse(base$var_57  <= quantile(base$var_57, 0.25,na.rm=T), "0-3",
+                                  ifelse(base$var_57 <= quantile(base$var_57, 0.50,na.rm=T)  , "4-7", 
+                                         ifelse(base$var_57 <= quantile(base$var_57, 0.75,na.rm=T)  ,"8-13", "14+"))))
+
+order <- c("0-3","4-7", "8-13","14+"  )
+base$CAT_var_57 <- factor(base$CAT_var_57, levels=order, ordered=TRUE)
 
 
 g1 <- ggplot(gerar_df(base$CAT_var_103), aes(x = Categoria)) +
@@ -190,10 +212,10 @@ g1 <- ggplot(gerar_df(base$CAT_var_103), aes(x = Categoria)) +
   scale_y_continuous(sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal()  +  
   theme(
     plot.title = element_text(hjust = 0.5,size=18),
-    axis.title.x = element_text(size = 14),  # Aumenta o tamanho do título do eixo x
-    axis.title.y = element_text(size = 14),  # Aumenta o tamanho do título do eixo 
+    axis.title.x = element_text(size = 14),  
+    axis.title.y = element_text(size = 14), 
     panel.border = element_rect(color = "black",fill=NA),
-    axis.text.x = element_text(angle = 45, hjust = 1,size=12,color='black'),
+    axis.text.x = element_text( size=12,color='black'),
     axis.text.y = element_text(size=12,color = "black")
   )
 
@@ -204,31 +226,31 @@ g2 <- ggplot(gerar_df(base$CAT_var_78), aes(x = Categoria)) +
   geom_line(aes(y = Taxa_Inadimplencia * 100, group = 1), color = "firebrick2", linewidth=1) +
   geom_point(aes(y = Taxa_Inadimplencia * 100), color = "firebrick2",size=2) +
   labs(x = "Categoria", y = "Representatividade (%)", 
-       title = "Valor total de vencimentos quitados\n antecipados nos últimos 2 anos") +
+       title = "Valor de vencimentos quitados\n antecipados nos últimos 2 anos") +
   scale_y_continuous(sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal()  +  
   theme(
     plot.title = element_text(hjust = 0.5,size=18),
-    axis.title.x = element_text(size = 14),  # Aumenta o tamanho do título do eixo x
-    axis.title.y = element_text(size = 14),  # Aumenta o tamanho do título do eixo 
+    axis.title.x = element_text(size = 14), 
+    axis.title.y = element_text(size = 14),  
     panel.border = element_rect(color = "black",fill=NA),
-    axis.text.x = element_text(angle = 45, hjust = 1,size=12,color='black'),
+    axis.text.x = element_text(size=12,color='black'),
     axis.text.y = element_text(size=12,color='black')
   )
 
 
-g3 <- ggplot(gerar_df(base$CAT_var_20), aes(x = Categoria)) +
-  geom_bar(aes(y = Representatividade.Freq * 100), stat = "identity", fill = "deepskyblue2",width = 0.5) +
+g3 <- ggplot(gerar_df(base$CAT_var_22), aes(x = Categoria)) +
+  geom_bar(aes(y = Representatividade.Freq * 100), stat = "identity", fill = "deepskyblue2",width = 0.3) +
   geom_line(aes(y = Taxa_Inadimplencia * 100, group = 1), color = "firebrick2", linewidth=1) +
   geom_point(aes(y = Taxa_Inadimplencia * 100), color = "firebrick2",size=2) +
   labs(x = "Categoria", y = "Representatividade (%)", 
-       title = "Média de dias de vencimentos quitados \n atrasados nos últimos 6 meses") +
+       title = "Atraso no pagamento \n nos últimos 6 meses") +
   scale_y_continuous(sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal() +  
   theme(
     plot.title = element_text(hjust = 0.5,size=18),
-    axis.title.x = element_text(size = 14),  # Aumenta o tamanho do título do eixo x
-    axis.title.y = element_text(size = 14),  # Aumenta o tamanho do título do eixo 
+    axis.title.x = element_text(size = 14),  
+    axis.title.y = element_text(size = 14),  
     panel.border = element_rect(color = "black",fill=NA),
-    axis.text.x = element_text(angle = 45, hjust = 1,size=12,color='black'),
+    axis.text.x = element_text( size=12,color='black'),
     axis.text.y = element_text(size=12,color='black')
   )
 
@@ -243,10 +265,10 @@ g4 <- ggplot(gerar_df(base$CAT_var_57), aes(x = Categoria)) +
   scale_y_continuous(sec.axis = sec_axis(~./100, name = "Taxa de Inadimplência (%)")) + theme_minimal()  +  
   theme(
     plot.title = element_text(hjust = 0.5,size=18),
-    axis.title.x = element_text(size = 14),  # Aumenta o tamanho do título do eixo x
-    axis.title.y = element_text(size = 14),  # Aumenta o tamanho do título do eixo 
+    axis.title.x = element_text(size = 14),  
+    axis.title.y = element_text(size = 14),  
     panel.border = element_rect(color = "black",fill=NA),
-    axis.text.x = element_text(angle = 45, hjust = 1,size=12,color='black'),
+    axis.text.x = element_text(size=12,color='black'),
     axis.text.y = element_text(size=12,color='black')
   )
 
