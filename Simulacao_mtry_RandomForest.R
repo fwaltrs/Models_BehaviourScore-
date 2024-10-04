02d05cb9319932670d638698df5b3815e40eea91
Fernanda Waltrs Freitas <96556228+fwaltrs@users.noreply.github.com>
2024-10-04 17:13:12 -0300

    Update Simulacao_mtry_RandomForest.R

--- Simulacao_mtry_RandomForest.R
+++ Simulacao_mtry_RandomForest.R
@@ -1,19 +1,13 @@
 n_features <- length(setdiff(names(validacao), "var_resposta"))
 hyper_grid <- expand.grid(
-  mtry = c(3,5,10,15,20,25,30,40,50), 
-  prediction.error = NA,
-  KS_treino = NA,
+  mtry = c(5,10,15,20), 
+  B = c(250,500,600),
+  max.depth = c(3,6,10),
   KS_validacao = NA,
   KS_teste = NA,
-  KS_total = NA,
-  auc_roc_treino = NA,
   auc_roc_validacao = NA,
-  auc_roc_teste = NA, 
-  auc_roc_total = NA, 
-  gini_treino = NA,
-  gini_validacao = NA,
-  gini_teste = NA,
-  gini_total = NA
+  auc_roc_teste = NA,
+  gini_validacao = NA
   
 )
 
@@ -23,6 +17,7 @@ for(i in seq_len(nrow(hyper_grid))) {
     formula         = var_resposta ~ ., 
     data            = treino, 
     mtry            = hyper_grid$mtry[i],
+    num.trees = 500, 
     importance = "impurity",
     replace=T,
     splitrule = "gini",
@@ -32,7 +27,6 @@ for(i in seq_len(nrow(hyper_grid))) {
     classification=T
   )
   
-  hyper_grid$prediction.error[i] <- fit$prediction.error
   
   ## com a amostra de validação
   previsoes <- predict(fit,data=validacao) 
@@ -43,14 +37,7 @@ for(i in seq_len(nrow(hyper_grid))) {
   auc_roc_validacao <- auc(roc_obj)
   gini_validacao <- 2 * auc_roc_validacao - 1 
   
-  
-  previsoes <- predict(fit,data=treino) 
-  score = previsoes$predictions[,1]*1000
-  treino$score = score
-  ks_treino = ks.test(treino[treino$var_resposta==0,]$score, treino[treino$var_resposta==1,]$score)$statistic   
-  roc_obj <- roc(treino$var_resposta, previsoes$predictions[,1])
-  auc_roc_treino <- auc(roc_obj)
-  gini_treino <- 2 * auc_roc_treino - 1 
+
   
   
   previsoes <- predict(fit,data=teste) 
@@ -59,30 +46,16 @@ for(i in seq_len(nrow(hyper_grid))) {
   ks_teste = ks.test(teste[teste$var_resposta==0,]$score, teste[teste$var_resposta==1,]$score)$statistic   
   roc_obj <- roc(teste$var_resposta, previsoes$predictions[,1])
   auc_roc_teste <- auc(roc_obj)
-  gini_teste <- 2 * auc_roc_teste - 1 
-  
-  
-  
-  previsoes <- predict(fit,data=base1) 
-  score = previsoes$predictions[,1]*1000
-  base1$score = score
-  ks_total = ks.test(base1[base1$var_resposta==0,]$score, base1[base1$var_resposta==1,]$score)$statistic   
-  roc_obj <- roc(base1$var_resposta, previsoes$predictions[,1])
-  auc_roc_total <- auc(roc_obj)
-  gini_total <- 2 * auc_roc_total - 1 
-  
+
   
   hyper_grid$KS_validacao[i] <- ks_validacao
-  hyper_grid$KS_treino[i] <- ks_treino
   hyper_grid$KS_teste[i] <- ks_teste
-  hyper_grid$KS_total[i] <- ks_total
-  hyper_grid$auc_roc_treino[i] <- auc_roc_treino
+
+
   hyper_grid$auc_roc_validacao[i] <- auc_roc_validacao
   hyper_grid$auc_roc_teste[i] <- auc_roc_teste
-  hyper_grid$auc_roc_total[i] <- auc_roc_total
+
   hyper_grid$gini_validacao[i] <- gini_validacao
-  hyper_grid$gini_treino[i] <- gini_treino
-  hyper_grid$gini_teste[i] <- gini_teste
-  hyper_grid$gini_total[i] <- gini_total
+
   
 }
